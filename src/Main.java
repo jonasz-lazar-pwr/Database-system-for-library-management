221518b2be24e7de179b23564d34f8a11708f99b
malinovskovy <k-malinowski@mail.com>
2023-10-20 19:01:55 +0200

    added insert into table

--- src/Main.java
+++ src/Main.java
@@ -12,8 +12,10 @@ public class Main {
         try {
             Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
             Statement statement = connection.createStatement();
-            String query = "SELECT * FROM Osoby";
 
+            // WYBIERZ WSZYSTKIE REKORDY I JE WYŚWIETL
+            System.out.println("1.");
+            String query = "SELECT * FROM Osoby";
             ResultSet resultSet = statement.executeQuery(query);
 
             while (resultSet.next()) {
@@ -26,7 +28,27 @@ public class Main {
                 System.out.println("ID: " + id + ", Imię: " + name + ", Nazwisko: " + surname + ", Wiek: " + age + ", Narodowość: " + nationality);
             }
 
+            // DODAJ NOWY REKORD
+//            System.out.println("2.");
+//            String queryInsert = "INSERT INTO Osoby (ID, Imię, Nazwisko, Wiek, Narodowość) VALUES (88, 'Jan', 'Kowalski', 30, 'polska')";
+//            statement.executeUpdate(queryInsert);
+
+            // PONOWNIE WYŚWIETL WSZYSTKIE REKORDY
+            System.out.println("3.");
+            ResultSet resultSet2 = statement.executeQuery(query);
+
+            while (resultSet2.next()) {
+                int id = resultSet2.getInt("ID");
+                String name = resultSet2.getString("Imię");
+                String surname = resultSet2.getString("Nazwisko");
+                int age = resultSet2.getInt("Wiek");
+                String nationality = resultSet2.getString("Narodowość");
+
+                System.out.println("ID: " + id + ", Imię: " + name + ", Nazwisko: " + surname + ", Wiek: " + age + ", Narodowość: " + nationality);
+            }
+
             resultSet.close();
+            resultSet2.close();
             statement.close();
             connection.close();
         } catch (Exception e) {
